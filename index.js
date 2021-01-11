102ae3c47852aaaa6c5cc119a147be75e71cb70b
mccalljoey <mccalljoey@gmail.com>
2021-01-11 22:41:20 +0000

    fixing the index

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+// const theExports = 
+module.exports = {
+  head,
+  tail,
+  middle
+};
