bed2e812730940d3d22c9b6c539acc3923727e57
mccalljoey <mccalljoey@gmail.com>
2021-01-11 22:57:16 +0000

    fixed the index again

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // index.js
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
+const head = require('../lotideOriginal/head');
+const tail = require('../lotideOriginal/tail');
+const middle = require('../lotideOriginal/middle');
 
-// const theExports = 
 module.exports = {
   head,
   tail,
